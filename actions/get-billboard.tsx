8f2fa1c160293536be78b2462cee58ddac4438ae
hemda74 <92821656+hemda74@users.noreply.github.com>
2023-11-17 10:22:04 +0200

    change gitbillboard endpoint

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -9,7 +9,4 @@ const getBillboard = async (id: string): Promise<Billboard> => {
 };
 
 export default getBillboard;
-NEXT_PUBLIC_API_URL =
-  "http://localhost:3001/api/192dbb95-cac7-423b-b184-495fde3a8b3f";
-NEXT_PUBLIC_API_URL_2 =
-  "http://localhost:3000/api/b809aefd-1305-430e-b03e-4f2329c93ade";
+
